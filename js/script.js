a5ae8375b3e7ecda2b5bbee74a33bacc36e0f677
BritishPandaChick <sbartley1080@gmail.com>
2020-03-11 22:06:18 -0400

    Add CSS code. Add remaining code for loop function

--- js/script.js
+++ js/script.js
@@ -15,6 +15,9 @@ window.onload = function(){
     tcanvas.width = W;
     tcanvas.height = H; //same as the image 
     var tc = tcanvas.getContext("2d");
+    //painting the canvas white before painting the image to deal with pngs 
+    tc.fillStyle = "white";
+    tc.fillRect(0, 0, W, H);
     //drawing the image on the canvas 
     tc.drawImage(sprite, 0, 0, W, H);
 
@@ -31,7 +34,7 @@ window.onload = function(){
         //converting the pixel into grayscale 
         gray = r*0.2126 + g*0.7152 + b*0.0722;
         //Overwriting the colordata array with grayscale values 
-        colordata[i] = colordata[i+1] = colordata[i+2] = gray;
+        //colordata[i] = colordata[i+1] = colordata[i+2] = gray;
 
         //text for ascii art 
         //blackish = dense characters like "W", "@"
@@ -57,17 +60,46 @@ window.onload = function(){
         //newlines and injection into dom 
         if(i != 0 && (i/4)%W == 0) { //if the pointer reaches the end of pixel-line
             ascii.appendChild(document.createTextNode(line));
+            //newline 
+            ascii.appendChild(document.createElement("br")); 
+            //emptying line for the next
+            line = "";
         }
 
         line += character;
     }
 
     //repainting the gray image 
-    tc.putImageData(pixels, 0, 0);
+    //tc.putImageData(pixels, 0, 0);
     //you can see the grayscale version of the sprite now 
     //injecting the canvas into the DOM 
-    sprite.parentNode.insertBefore(tcanvas, sprite);
+    //sprite.parentNode.insertBefore(tcanvas, sprite);
     //you can see the canvas now with the image 
 
+    //animation time 
+    var frames = 10; //sprite got 10 frames 
+    var container = document.getElementById("container");
+    //width of container should allow only 1 frame to be seen 
+    var frame_width = parseInt(window.getComputedStyle(container).width/frames);
+    //window.getComputedStyle is supported in Chrome, FF, Opera, and IE9+ 
+    //The width has "px" at the end so parseInt is used to remove that 
+    container.style.width = frame_width + "px";
+
+    //We will change the margin-left of ascii to move it. 
+    ascii.style.marginLeft = "0";
+
+    this.setInterval(loop, 1000/10);
+
+    function loop(){
+        var current_ml = parseFloat(ascii.style.marginLeft);
+        //if the ascii reaches the last frame (9th in this case)
+        //margin needs to be reset to 0
+        //frame_width * (10-1) * -1(because we are taking the margin negative) 
+        if(current_ml == frame_width*(frames-1)*-1){
+            ascii.style.marginLeft = "0"; 
+        } else {
+            ascii.style.marginLeft = (current_ml - frame_width) + "px";
+        }
+    }
 }
 
